b31d0297379f488fe8d051271f31174426c52727
yaramiu <yaramiu@gmail.com>
2024-07-03 15:24:58 -0400

    Add append method to linked list class

--- linked-list.js
+++ linked-list.js
@@ -1,1 +1,18 @@
-class LinkedList {}
+import { Node } from "./node.js";
+
+export class LinkedList {
+  list = null;
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.list) {
+      this.list = newNode;
+    } else {
+      let currentNode = this.list;
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+  }
+}
--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-class Node {
+export class Node {
   constructor(value = null, nextNode = null) {
     this.value = value;
     this.nextNode = nextNode;
